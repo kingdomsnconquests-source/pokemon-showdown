96dfdc7d0eb2e9299981c61cd69b693fb8d365f5
ImTotallyNotOnGitHub <franzandrew16@gmail.com>
2025-12-24 02:05:41 -0500

    Updates

--- config/formats.ts
+++ config/formats.ts
@@ -26,7 +26,7 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	{
 		name: "[Gen 9] KnC: Vanilla+ Season 1 Singles",
 		desc: `The format for the first KnC using a personally modified showdown client.`,
-		mod: 'conquest',
+		mod: 'gen9',
 		ruleset: ['Standard Conquest', 'Evasion Abilities Clause', 'Sleep Moves Clause', '!Sleep Clause Mod'],
 		banlist: [
 			'Spirit'
